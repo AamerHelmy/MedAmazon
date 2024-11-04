9d2e349b90448c5c69ca567abf75f04b5a2b9427
AamerHelmy <Aamer.helmy@outlook.com>
2024-11-04 11:31:22 +0200

    test main

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"fuzzyTest/db"
-	"fuzzyTest/medName"
+	// "fuzzyTest/medName"
 	"fuzzyTest/services"
 	"log"
 	"time"
@@ -19,40 +19,40 @@ func main() {
 	gdb := db.GetGdb()
 
 	itemSv := services.NewItemService(gdb)
-	// vendorSv := services.NewVenderService(gdb)
+	vendorSv := services.NewVenderService(gdb)
 
 	// upload items with excel(xlsx)
-	itemsUpdateReport, err := itemSv.UpdateItemsFromExcel("zpath/excel/Items2.xlsx")
+	itemsUpdateReport, err := itemSv.UpdateItemsFromExcel("zpath/excel/Items.xlsx")
 	if err != nil {
 		log.Printf("error while CreateBulk: %v", err)
 	}
 	// print result
 	itemsUpdateReport.Print()
 
-	text := "  ketogan    50mg       "
-	textClean := medName.Clean(text)
-	log.Printf("(%v)",textClean)
+	// text := "  ketogan    50mg       "
+	// textClean := medName.Clean(text)
+	// log.Printf("(%v)",textClean)
 
 	// create new vendor
-	// ven1, err := vendorSv.AddNew("الاهرام")
-	// if err != nil {
-	// 	log.Fatal("error while create vendor: ", err)
-	// }
+	ven1, err := vendorSv.AddNew("الاهرام")
+	if err != nil {
+		log.Fatal("error while create vendor: ", err)
+	}
 
-	// // upload vendor's offer
-	// vendorOfferReport2, err := vendorSv.UpdateOffersFromExcel("zpath/excel/الاهرام.xlsx", ven1)
-	// if err != nil {
-	// 	log.Printf("error while updating vendor offer: %v", err)
-	// }
-	// // print result
-	// vendorOfferReport2.Print()
+	// upload vendor's offer
+	vendorOfferReport2, err := vendorSv.UpdateOffersFromExcel("zpath/excel/الاهرام.xlsx", ven1)
+	if err != nil {
+		log.Printf("error while updating vendor offer: %v", err)
+	}
+	// print result
+	vendorOfferReport2.Print()
 
-	// // match all items
-	// matchReport, err := vendorSv.AutoLinkItems()
-	// if err != nil {
-	// 	log.Printf("error while match vendor's items offer: %v", err)
-	// }
-	// matchReport.Print()
+	// match all items
+	matchReport, err := vendorSv.AutoLinkItems()
+	if err != nil {
+		log.Printf("error while match vendor's items offer: %v", err)
+	}
+	matchReport.Print()
 
 	fmt.Printf("\napp take time: %v ", time.Since(start))
 	fmt.Println("ending app ...")
